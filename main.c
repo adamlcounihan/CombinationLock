2bb2174c2443c378ceab99210ec7b5753077ce91
adamlcounihan <137104673+adamlcounihan@users.noreply.github.com>
2024-03-27 22:39:39 +0000

    Upload project

---
+++ main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main () {
+    //variables to store guesses
+    int input1;
+    int input2;
+    int input3;
+
+    //number of numbers in correct or wrong spots
+    int correct_spot;
+    int wrong_spot;
+
+
+    int tries = 15;
+
+    //numbers are randomly generated
+    srand(time(NULL));
+    int num1 = rand() % 9+1;
+    int num2 = rand() % 9+1;
+    int num3 = rand() % 9+1;
+
+
+
+
+    //ensures that there are no repeat numbers generated
+    while(num1 == num2 || num1==num3){
+        num1 = rand() % 9+1;
+        num2 = rand() % 9+1;
+        num3 = rand() % 9+1;
+    }
+    while(num2 == num1 || num2==num3){
+        num1 = rand() % 9+1;
+        num2 = rand() % 9+1;
+        num3 = rand() % 9+1;
+    }
+
+
+    int code[3] = {num1, num2, num3};
+
+
+    while(tries != 0){
+        tries = tries - 1;
+
+        //resets values to 0
+        correct_spot = 0;
+        wrong_spot = 0;
+
+
+        printf("Guess the three digit code \n");
+        printf("Input in the form of: 1,2,3");
+        sleep(2);
+        fflush(stdin);
+        scanf("%d,%d,%d", &input1, &input2, &input3);
+
+
+        //if an input is correct
+        if (input1 == num1){
+            correct_spot = correct_spot + 1;
+        }
+        if (input2 == num2){
+            correct_spot = correct_spot + 1;
+        }
+        if (input3 == num3){
+            correct_spot = correct_spot + 1;
+        }
+
+        //if an input is wrong, but matches the value of another random number slot
+        if (input1 != num1){
+            if (input1 == num2 || input1 == num3){
+                wrong_spot = wrong_spot + 1;
+            }
+        }
+        if (input2 != num2){
+            if (input2 == num1 || input2 == num3){
+                wrong_spot = wrong_spot + 1;
+            }
+        }
+        if (input3 != num3){
+            if (input3 == num2 || input3 == num1){
+                wrong_spot = wrong_spot + 1;
+            }
+        }
+
+        sleep(2);
+        printf("%d digits are in the correct spot \n", correct_spot);
+        sleep(1);
+        printf("%d correct digits are in the wrong spot \n", wrong_spot);
+        sleep(1);
+        printf("You have %d tries left \n \n \n", tries);
+        sleep(1);
+
+        //if user guessed correctly
+        if (input1 == num1 && input2 == num2 && input3 == num3){
+            printf("Correct number inputted, proceed.");
+            return 0;
+        }
+
+    }
+    //if user ran out of tries and lost
+    printf("The code was %d,%d,%d. You failed. \n", code[0], code[1], code[2]);
+    return 1;
+
+}
+
